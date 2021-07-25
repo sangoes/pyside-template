05353d3f620075abb04d94fc08e8d6c07372cb95
jerrychir <jerrychir@jerrychirs-MacBook-Pro.local>
2021-07-25 21:48:28 +0800

    fhange

--- lancher.py
+++ lancher.py
@@ -6,9 +6,9 @@ from src.views.lancher_widget import LancherWidget
 # main
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
-
+     
     widget = LancherWidget()
     widget.resize(800, 600)
     widget.show()
-
+    
     sys.exit(app.exec())
